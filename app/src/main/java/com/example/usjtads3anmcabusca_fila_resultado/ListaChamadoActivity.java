689f9f714ae43cfb8ef36af3bfb0bedaff467d74
arqdsis <arqdsis@MOOLI10239.animaedu.intranet>
2019-03-11 20:41:00 -0300

    Criada estrutura

---
+++ app/src/main/java/com/example/usjtads3anmcabusca_fila_resultado/ListaChamadoActivity.java
@@ -0,0 +1,44 @@
+package com.example.usjtads3anmcabusca_fila_resultado;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class ListaChamadoActivity extends AppCompatActivity {
+    ArrayList<String> lista;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_chamado);
+    }
+
+    
+
+    public ArrayList<String> geraListaChamados(){
+        ArrayList<String> lista = new ArrayList<>();
+        lista.add("Desktops: Computador da secretária quebrado.");
+        lista.add("Telefonia: Telefone não funciona.");
+        lista.add("Redes: Manutenção no proxy.");
+        lista.add("Servidores: Lentidão generalizada.");
+        lista.add("Novos Projetos: CRM");
+        lista.add("Manutenção Sistema ERP: atualizar versão.");
+        lista.add("Novos Projetos: Rede MPLS");
+        lista.add("Manutenção Sistema de Vendas: incluir pipeline.");
+        lista.add("Manutenção Sistema ERP: erro contábil");
+        lista.add("Novos Projetos: Gestão de Orçamento");
+        lista.add("Novos Projetos: Big Data");
+        lista.add("Manoel de Barros");
+        lista.add("Redes: Internet com lentidão");
+        lista.add("Novos Projetos: Chatbot");
+        lista.add("Desktops: troca de senha");
+        lista.add("Desktops: falha no Windows");
+        lista.add("Novos Projetos: ITIL V3");
+        lista.add("Telefonia: liberar celular");
+        lista.add("Telefonia: mover ramal");
+        lista.add("Redes: ponto com defeito");
+        lista.add("Novos Projetos: ferramenta EMM");
+        return lista;
+    }
+}
